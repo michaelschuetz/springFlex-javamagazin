93e193b88d6c3456927ec6bce27db9a65e658e54
michaelschuetz <michaelschuetz83@gmail.com>
2010-09-26 18:07:19 +0200

    initial commit. - app uses Spring3, Flex 3.5. - works with Maven3

---
+++ jm-war/src/main/java/com/acme/service/HelloWorldService.java
@@ -0,0 +1,15 @@
+package com.acme.service;
+
+import org.springframework.flex.remoting.RemotingDestination;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author <a href="mailto:michaelschuetz83@gmail.com">Michael Schuetz</a>
+ */
+@Service("helloWorldService")
+@RemotingDestination
+public class HelloWorldService {
+    public String say(String name) {
+        return "Hello3 " + name;
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,12 @@
+				springFlex-javamagazin
+				======================
+
+Source Code for Spring3 Flex4 article in JavaMagazin.
+
+Current:
+Spring 3.0.4
+Flex 3.5
+Maven 3
+
+TODO:
+* Flex 4
